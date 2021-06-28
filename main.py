c10cc9b9be9b8dee3d22dab0000a5e29fc231895
Lisheng <zhenglisheng@gmail.com>
2021-06-28 10:53:38 +0800

    Refactoring code

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -17,77 +18,34 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 MAX_PAGE_SIZE = 1000
 FILE_FIELDS = "name, modifiedTime, id, trashed, ownedByMe, md5Checksum"
 
-# For application data log file naming
+# For reporting
 date = datetime.now().strftime("%Y_%m_%d-%I_%M_%S%p")
-APP_DATA_LOG_FOLDER = 'app_data_logs'
+REPORT_FOLDER = 'reports'
 
 LOGGER_NAME = 'GDrive Duplicate Remover'
+
+# New fields for status tracking of files.
+# We can leverage the existing `trashed` field of files, but it may not be a
+# good idea as we don't have the control of the field and Google could rename
+# or remove the field one day.
+# Another benefit of adding new fields, except having control, is it helps
+# auditing/failure analysis by have different status field.
+TO_REMOVE = 'to_remove'
+REMOVED = 'removed'
 
 
 def main():
     api_client = get_gdrive_api_client()
 
-    # Call the Drive v3 API to get list of files
-    hash_map = fetch_gdrive_files(api_client)
-
+    hash_map = fetch_files_from_gdrive(api_client)
     logger.info(f'Total number of md5Checksum entries: {len(hash_map)}')
+    _produce_candidate_files_report(hash_map)
 
-    # Log candidate files to a file
-    logfile = os.path.join(APP_DATA_LOG_FOLDER, f'candidate_files-{date}.log')
-    os.makedirs(os.path.dirname(logfile), exist_ok=True)
-    with open(logfile, 'w') as log_file:
-        for md5_checksum, file_list in hash_map.items():
-            log_file.write(md5_checksum + '\n')
-            for file in file_list:
-                log_file.write(f"\tName: {file['name']} "
-                               f"- modifiedTime: {file['modifiedTime']} "
-                               f"- ID: {file['id']} "
-                               f"- ownedByMe: {file['ownedByMe']} "
-                               f"- trashed: {file['trashed']} "
-                               f"- md5Checksum: {file['md5Checksum']}\n")
-    
-    # Loop through the hash map
-    # For each file list under a md5Checksum,
-    #   if num of files > 1,
-    #   loop through the file list to mark which files to soft delete i.e. trash
-    #   loop through the file list again to trash marked files
-    to_trash_files_log = open(f'to_trash_files-{date}.log', 'a')
-    trashed_files_log = open(f'trashed_files-{date}.log', 'a')
-    for md5_checksum, file_list in hash_map.items():
-        if len(file_list) > 1:
-            # Assuming the first file is the most recent file
-            most_recent_file = file_list[0]
-            # Loop through the file list to
-            #   1) mark trash_mark for each file
-            #   2) find out which file is the most recent file
-            for file in file_list:
-                file['trashed'] = True
-                if file['modifiedTime'] > most_recent_file['modifiedTime']:
-                    most_recent_file = file
-            # Mark trash_mark for the most recent file
-            most_recent_file['trashed'] = False
-            # Loop through the file list to delete duplicates
-            to_trash_files_log.write(md5_checksum + '\n')
-            for file in file_list:
-                log_str = (f"\tName: {file['name']} "
-                           f"- modifiedTime: {file['modifiedTime']} "
-                           f"- ID: {file['id']} "
-                           f"- ownedByMe: {file['ownedByMe']} "
-                           f"- trashed: {file['trashed']} "
-                           f"- md5Checksum: {file['md5Checksum']}"
-                           f"\n")
-                to_trash_files_log.write(log_str)
-                if file['trashed'] is True:
-                    logger.info(f'Trash file: {file}')
-                    trashed_files_log.write(log_str)
+    _mark_duplicates(hash_map)
+    _produce_files_for_removal_report(hash_map)
 
-                    results = api_client.files().update(fileId=file['id'],
-                                                     body={'trashed': True}
-                                                     ).execute()
-                    logger.info(f'Trash file result: {results}')
-    # Close file resources
-    to_trash_files_log.close()
-    trashed_files_log.close()
+    remove_duplicates_from_gdrive(api_client, hash_map)
+    _produce_files_removed_report(hash_map)
 
 
 def get_gdrive_api_client():
@@ -120,13 +78,16 @@ def get_gdrive_api_client():
     return service
 
 
-def fetch_gdrive_files(api_client):
+def fetch_files_from_gdrive(api_client):
     """Fetches list of files from Google Drive.
 
     Fetches only files that have md5Checksum. Files that does not have
     md5checksum: folders, Google Docs, Google Sheets, Google Slides,
     and other Google Office files. It also excludes files that are not owned
     by the user or have already been trashed.
+
+    Args:
+        api_client: Google Drive API client.
 
     Returns:
         A dict using files' md5Checksum as the keys. The value for each key
@@ -157,7 +118,7 @@ def fetch_gdrive_files(api_client):
         logger.info(f'next_page_token: {next_page_token}')
         files = results.get('files', [])
         if not files:
-            logger.info('No files found.')
+            logger.info('No suitable files found in your Google Drive.')
         else:
             for file in files:
                 if (file.get('md5Checksum') is not None and
@@ -175,6 +136,85 @@ def fetch_gdrive_files(api_client):
     return hash_map
 
 
+def remove_duplicates_from_gdrive(api_client, hash_map):
+    """Remove duplicate files from Google Drive.
+
+    It actually trash duplicate files by updating a duplicate file's metadata
+    field `trashed` to be true.
+
+    Args:
+        api_client: Google Drive API client.
+        hash_map: a dict contains info about md5Checksum and its related files
+
+    Returns:
+        None
+    """
+    for file_list in hash_map.values():
+        for file in file_list:
+            if file.get(TO_REMOVE) is True:
+                logger.info(f'Trashing file: {file}')
+                result = api_client.files().update(fileId=file['id'],
+                                                   body={'trashed': True}
+                                                   ).execute()
+                logger.info(f'Trashing file done - response: {result}')
+                file[REMOVED] = True
+
+
+def _mark_duplicates(hash_map):
+    for md5_checksum, file_list in hash_map.items():
+        # Loop through the file list to
+        # 1) mark files to be removed
+        # 2) find out most recent file to keep
+        if len(file_list) > 1:
+            most_recent_file = file_list[0]
+            for file in file_list:
+                file[TO_REMOVE] = True
+                if file['modifiedTime'] > most_recent_file['modifiedTime']:
+                    most_recent_file = file
+            most_recent_file[TO_REMOVE] = False
+
+
+def _produce_candidate_files_report(hash_map):
+    report_file = os.path.join(REPORT_FOLDER, f'Candidate_Files-{date}.log')
+    _produce_report(hash_map, report_file)
+
+
+def _produce_files_for_removal_report(hash_map):
+    new_hash_map = {}
+    for md5_checksum, file_list in hash_map.items():
+        new_list = [file for file in file_list if file.get(TO_REMOVE) is True]
+        if len(new_list) > 0:
+            new_hash_map[md5_checksum] = new_list
+
+    report_file = os.path.join(REPORT_FOLDER, f'Files_To_Remove-{date}.log')
+    _produce_report(new_hash_map, report_file)
+
+
+def _produce_files_removed_report(hash_map):
+    new_hash_map = {}
+    for md5_checksum, file_list in hash_map.items():
+        new_list = [file for file in file_list if file.get(REMOVED) is True]
+        if len(new_list) > 0:
+            new_hash_map[md5_checksum] = new_list
+
+    report_file = os.path.join(REPORT_FOLDER, f'Files_Removed-{date}.log')
+    _produce_report(new_hash_map, report_file)
+
+
+def _produce_report(hash_map, report_file):
+    os.makedirs(os.path.dirname(report_file), exist_ok=True)
+    with open(report_file, 'w') as f:
+        for md5_checksum, file_list in hash_map.items():
+            f.write(f"md5Checksum: {md5_checksum}\n")
+            for file in file_list:
+                f.write(f"\tName: {file['name']} "
+                        f"- modifiedTime: {file['modifiedTime']} "
+                        f"- ID: {file['id']} "
+                        f"- ownedByMe: {file['ownedByMe']} "
+                        f"- trashed: {file['trashed']} "
+                        f"- md5Checksum: {file['md5Checksum']}\n")
+
+
 if __name__ == '__main__':
     # Config logging
     with open('logging_config.json', 'r') as logging_config_file:
@@ -182,4 +222,5 @@ if __name__ == '__main__':
         dictConfig(logging_config)
     logger = logging.getLogger(LOGGER_NAME)
 
+    # Run the application
     main()
